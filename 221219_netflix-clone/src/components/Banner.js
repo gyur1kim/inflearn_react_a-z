cbe30a5a10d82bbc04d79de482bdb4b196b32eed
gyur1kim <qri1114@gmail.com>
2022-12-27 23:55:36 +0900

    221226_Styled Component를 이용한 movie 예고편

--- 221219_netflix-clone/src/components/Banner.js
+++ 221219_netflix-clone/src/components/Banner.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import styled from 'styled-components';
 
 // axios 모듈을 사용하는 것이 아니라 우리가 만든 instance를 사용!
 import axios from '../api/axios'
@@ -8,6 +9,7 @@ import './Banner.css'
 
 function Banner(props) {
   const [movie, setMovie] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -33,28 +35,78 @@ function Banner(props) {
     return text?.length > n? text.slice(0, n) + '...' : text
   }
 
-  return (
-    <header
-      className="banner"
-      style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")`,
-        backgroundPosition: "top center",
-        backgroundSize: "cover"
-      }}
-    >
-      <div className="banner__contents">
-        <h1 className="banner__title">{ movie.title || movie.name || movie.original_name }</h1>
-        <div className="banner__buttons">
-          <button className="banner__button play">Play</button>
-          <button className="banner__button info">More Information</button>
+  // play 버튼을 누른 경우
+  if (isClicked) {
+    return (
+      <Container>
+        <HomeContainer>
+          <Iframe
+            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            title="YouTube video player"
+            allow="autoplay;"
+            allowFullScreen></Iframe>
+        </HomeContainer>
+      </Container>
+    );
+  }
+  // play 버튼을 누르지 않은 경우
+  else {
+    return (
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")`,
+          backgroundPosition: "top center",
+          backgroundSize: "cover"
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">{ movie.title || movie.name || movie.original_name }</h1>
+          <div className="banner__buttons">
+            <button className="banner__button play" onClick={() => setIsClicked(true)}>Play</button>
+            <button className="banner__button info">More Information</button>
+          </div>
+          <h1 className="banner__description">{truncate(movie.overview, 100)}</h1>
         </div>
-        <h1 className="banner__description">{truncate(movie.overview, 100)}</h1>
-      </div>
 
-      <div className="banner--fadeBottom" />
+        <div className="banner--fadeBottom" />
 
-    </header>
-  );
+      </header>
+    );
+  }
 }
+
+// CSS in JS 이용 -> 컴포넌트 생성
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+  
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`
+
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`
 
 export default Banner;
