bba5b79c1b08df893cb2f021beae6be193f80225
gyur1kim <qri1114@gmail.com>
2023-01-06 10:48:40 +0900

    feat: Search 시 검색된 영화 렌더링
    - searchResult에 값이 있으면 map을 돌며 영화 포스터 렌더링
    - searchResult에 값이 없으면 검색 결과가 없다는 화면 출력

--- 221219_netflix-clone/src/pages/SearchPage/index.js
+++ 221219_netflix-clone/src/pages/SearchPage/index.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import { useLocation } from 'react-router-dom';
 
 import axios from '../../api/axios'
+import './SearchPage.css'
 
 export default function SearchPage(props) {
 
@@ -24,7 +25,6 @@ export default function SearchPage(props) {
       const request = await axios.get(
         `/search/multi?include_adult=false&query=${searchTerm}`
       )
-      console.log(request);
       setSearchResult(request.data.results);
     }
     catch (error) {
@@ -32,7 +32,33 @@ export default function SearchPage(props) {
     }
   }
 
-  return (
-    <div></div>
-  );
+  console.log(searchResult)
+
+  const renderSearchResults = () => {
+    return searchResult.length > 0? (
+      <section className="search-container">
+        {searchResult.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== "person") {
+            const movieImageUrl = `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`;
+            return (
+              <div className="movie" key={`movie__column-${movie.id}`}>
+                <div className="movie__column-poster">
+                  <img src={movieImageUrl} alt="movie poster" className="movie__poster" />
+                </div>
+              </div>
+            );
+          }
+        })}
+      </section>
+    ) :
+    (
+      <section className="no-results">
+        <div className="no-results__text">
+          <p>"{searchTerm}" 와/과 관련된 영화가 존재하지 않습니다</p>
+        </div>
+      </section>
+    )
+  }
+
+  return renderSearchResults();
 }
