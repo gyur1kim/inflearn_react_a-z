f793e135caaffc0e89f3ce00386514f051b106e7
gyur1kim <qri1114@gmail.com>
2023-01-09 13:45:45 +0900

    refactor: Swiper 모듈을 이용해 Row 컴포넌트 수정
    - Swiper 모듈을 이용하여 Row의 포스터들이 슬라이드 기능으로도 움직임
    - breakpoints를 활용해 화면의 크기에 따라 Row에 보이는 포스터의 갯수가 달라짐

--- 221219_netflix-clone/src/components/Row.js
+++ 221219_netflix-clone/src/components/Row.js
@@ -1,4 +1,10 @@
 import React, {useEffect, useState} from 'react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 import axios from '../api/axios';
 import './Row.css'
@@ -31,48 +37,58 @@ function Row({title, id, isLargeRow, fetchUrl}) {
       {/* title */}
       <h2>{title}</h2>
 
-      {/* 캐로셀 구현 */}
-      <div className="slider">
-        <div className="slider__arrow-left">
-          {/* {"<"}로 표현한 이유 : 안그러면 태그로 인식해서 에러가 남 */}
-          <span
-            className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft -= window.innerWidth -80
-            }}
-          >
-            {"<"}
-          </span>
-        </div>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+
+        // 몇 개씩 보여줄건지!
+        // 반응형 웹페이지에서는 breakpoints를 이용함
+        // slidesPerView={3}
+        breakpoints={{
+          1378: {
+            slidesPerView: 6,
+            slidesPerGroup: 6      // 한 번에 몇 개씩 넘길 것인지
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3
+          }
+        }}
+        navigation                              // arrow btn
+        pagination={{ clickable: true }}
+        // scrollbar={{ draggable: true }}
+        loop={true}                            // 무한반복
+        // onSwiper={(swiper) => console.log(swiper)}
+        // onSlideChange={() => console.log('slide change')}
+      >
 
         <div id={id} className="row__posters">
           {/* 영화 포스터 */}
           {movies.map((movie) => {
             return (
-              <img
-                key={movie.id}
-                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                src={`${BASE_URL}${isLargeRow? movie.poster_path : movie.backdrop_path}`}
-                loading="lazy"
-                alt={movie.name}
-
-                onClick={() => handleClick(movie)}
-              />
+              <SwiperSlide key={movie.id}>
+                <img
+                  className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                  src={`${BASE_URL}${isLargeRow? movie.poster_path : movie.backdrop_path}`}
+                  loading="lazy"
+                  alt={movie.name}
+                  onClick={() => handleClick(movie)}
+                />
+              </SwiperSlide>
             )
           })}
         </div>
 
-        <div className="slider__arrow-right">
-          <span
-            className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft += window.innerWidth -80
-            }}
-          >
-            {">"}
-          </span>
-        </div>
-      </div>
+      </Swiper>
 
       {/* ModalOpen이 true면 같이 렌더링됨 */}
       { modalOpen && (
