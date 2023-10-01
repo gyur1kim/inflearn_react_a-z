b99dcdf3980cfcf0a15bb3ed5a1dccdd1f505684
gyur1kim <qri1114@gmail.com>
2023-10-01 23:45:45 +0900

    :sparkles: md파일 읽어오기
    - node.js의 process.cwd() 메서드를 이용해 현재 디렉토리를 불러와 posts를 조회함
    - gray-matter 모듈을 통해 md파일의 컨텐츠를 객체로 변환

--- 230110_nextjs-app/lib/post.ts
+++ 230110_nextjs-app/lib/post.ts
@@ -2,9 +2,17 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 
+type PostDataType = {
+  id: string,
+  content: string,
+  data: {
+    date: string,
+    title: string
+  }
+}
 const postsDirectory = path.join(process.cwd(), 'posts');
 // process.cwd()의 값
-// C:\Users\SSAFY\Desktop\inflearn-react\230110_nextjs-app
+// C:\Users\SSAFY\Desktop\inflearn-react\230110_nextjs-app\
 
 export function getSortedPostsData() {
   // posts 파일 이름 잡아주기
@@ -12,9 +20,28 @@ export function getSortedPostsData() {
   // 결과 : [pre-rendering.md, ssg-ssr.md]
 
   const allPostsData = fileNames.map((fileName) => {
+    // 파일 이름을 id로 이용하기
     const id = fileName.replace(/\.md$/, '');
     const fullPath = path.join(postsDirectory, fileName);
     const fileContents = fs.readFileSync(fullPath, 'utf-8');
+    // gray-matter 라이브러리를 이용하면, md파일을 객체로 convert
+    /**
+     * =====  md file  =====
+     * ---
+     * title: hello
+     * slug: home
+     * ---
+     * <h1>Hello</h1>
+     *
+     * =====  convert  =====
+     * {
+     *   content: '<h1>Hello</h1>'
+     *   data: {
+     *     title: 'hello',
+     *     slug: 'home'
+     *   }
+     * }
+     */
     const matterResult = matter(fileContents);
 
     return {
@@ -24,8 +51,8 @@ export function getSortedPostsData() {
   })
 
   // 추출한 posts를 날짜별로 sort하고 return
-  return allPostsData.sort((a, b) => {
-    if (a.data < b.data) return 1
+  return allPostsData.sort((a: PostDataType, b: PostDataType) => {
+    if (a.data.date < b.data.date) return 1
     else return -1
   })
 }
